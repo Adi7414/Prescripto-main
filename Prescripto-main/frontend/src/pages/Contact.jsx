bf1657e3b50ce0f494ba7daf4a71de1abeba9b3b
Aditya Kumar <adityakrblr4@gamil.com>
2024-12-08 23:31:18 +0530

    second commit

---
+++ Prescripto-main/frontend/src/pages/Contact.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import {assets} from '../assets/assets'
+
+const Contact = () => {
+  return (
+    <div>
+      
+      <div className='text-center tex-2xl pt-10 text-gray-500'>
+        <p>CONTACT <span className='text-gray-700 font-semibold'>US</span></p>
+      </div>
+
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
+        <img className='w-full max-w-[360px]' src={assets.contact_image} alt="" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
+          <p className='text-gray-500'>54709 Church Street <br /> Plot 145, Bangalore, Karnataka</p>
+          <p className='text-gray-500'>Tel: +91-9845127520 <br /> Email: contact@prescripto.com</p>
+          <p className='font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO</p>
+          <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Contact
